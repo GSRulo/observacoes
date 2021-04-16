cc14e09f1c823f0d4df62a1e9a0dd27434721354
Gabriel Santos Rulo <55097727+GSRulo@users.noreply.github.com>
2021-04-16 19:36:24 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ app.use(express.json());
 const axios = require('axios');
 
 const observacoesPorLembreteId = {};
+
+const funcoes = {
+    observacoesClassificada: (observacao) => {
+        const observacoes = observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+        obsParaAtualizar.status = observacao.status;
+
+        axios.post('http://localhost:10000/eventos', {
+            tipo: "observacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        });
+    }
+}
+
 const {
     v4: uuidv4
 } = require('uuid');
@@ -18,7 +37,8 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         observacoesPorLembreteId[req.params.id] || [];
     observacoesDoLembrete.push({
         id: idObs,
-        texto
+        texto,
+        status: 'aguardando'
     });
 
     observacoesPorLembreteId[req.params.id] =
@@ -28,7 +48,8 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
         dados: {
             id: idObs,
             texto,
-            lembreteId: req.params.id
+            lembreteId: req.params.id,
+            status: 'aguardando'
         }
     })
     res.status(201).send(observacoesDoLembrete);
@@ -36,6 +57,15 @@ app.put('/lembretes/:id/observacoes', async (req, res) => {
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
     res.send(observacoesPorLembreteId[req.params.id] || []);
+});
+
+app.post("/eventos", (req, res) => {
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+    res.status(200).send({
+        msg: "OK"
+    });
 });
 
 app.listen(4000, (() => {
